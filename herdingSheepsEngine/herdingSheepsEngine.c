2ead0c53c4914592a08c4ac97038f42203d43c3a
John Sharp <johncolinsharp@gmail.com>
2020-04-10 11:24:05 +0100

    removed unused code

--- herdingSheepsEngine/herdingSheepsEngine.c
+++ herdingSheepsEngine/herdingSheepsEngine.c
@@ -17,77 +17,6 @@ void herdingSheepsPreRender(engine * e)
 {
     herdingSheepsEngine * hsEng = e->owner;
     HSStateMachineProcessInput(hsEng->mainStateMachine);
-
-    // if (isStateActive(KEYPRESS_STATE_FRAME_PAUSE))
-    // {
-    //     engineIsPaused(e) ? engineUnpause(e) : enginePause(e);
-    //     deactivateState(KEYPRESS_STATE_FRAME_PAUSE);
-    // }
-    // if (engineIsPaused(e))
-    // {
-    //     Uint32 time = SDL_GetTicks();
-    //     static Uint32 lastTime = 0;
-    //     if (isStateActive(KEYPRESS_STATE_FORWARD_FRAME))
-    //     {
-    //         if (isStateActive(KEYPRESS_STATE_FORWARD_FRAME_MULTIPLIER))
-    //         {
-    //             if (time - lastTime > KEYPRESS_REFRESH_MS)
-    //             {
-    //                 lastTime = time;
-    //                 engineAdvanceFrames(e, 10);
-    //             }
-    //         }
-    //         else
-    //         {
-    //             if (time - lastTime > KEYPRESS_REFRESH_MS)
-    //             {
-    //                 lastTime = time;
-    //                 engineAdvanceFrames(e, 1);
-    //             }
-    //         }
-    //     }
-    //     else
-    //     {
-    //         lastTime = 0;
-    //     }
-    // }
-}
-
-STARTUP_CLICK_STATE startupClickState;
-void startUpClicksCB(jint x, jint y, void * owner)
-{
-    // herdingSheepsEngine * eng = (herdingSheepsEngine *)owner;
-    // if (startupClickState == STARTUP_CLICK_STATE_POSITION_SELECT)
-    // {
-    //     eng->bluePoint.ca.shape.point.v[0] = x;
-    //     eng->bluePoint.ca.shape.point.v[1] = (600 - y);
-
-    //     startupClickState = STARTUP_CLICK_STATE_VELOCITY_SELECT;
-    // }
-    // else if (startupClickState == STARTUP_CLICK_STATE_VELOCITY_SELECT)
-    // {
-    //     eng->bluePoint.ca.vel.v[0] = x - eng->bluePoint.ca.shape.point.v[0];
-    //     eng->bluePoint.ca.vel.v[1] = (600 - y) - eng->bluePoint.ca.shape.point.v[1];
-    //     eng->bluePoint.frameStart = eng->engine->currentFrame;
-
-    //     // calculate next collision frame here
-    //     wallActorList * wallNode = NULL;
-    //     for (wallNode = eng->walls; wallNode != NULL; wallNode = wallNode->next)
-    //     {
-    //         wallActor * wall = wallNode->val;
-
-    //         int frame = -1;
-    //         if (calculateNextCollisionFrame(
-    //                     &frame, &eng->bluePoint.ca.vel,
-    //                     &eng->bluePoint.ca, &wall->ca) == COLL_FRAME_CALC_NO_COLLISION)
-    //         {
-    //             printf("no collision detected\n\n");
-    //             frame = -1;
-    //         }
-    //         eng->bluePoint.ca.collFrame = frame;
-    //         wall->ca.collFrame = frame;
-    //     }
-    // }
 }
 
 herdingSheepsEngine * initHerdingSheepsEngine(herdingSheepsEngine * eng)
@@ -136,31 +65,6 @@ herdingSheepsEngine * initHerdingSheepsEngine(herdingSheepsEngine * eng)
 
     // setup other actor list
     eng->otherActorList = NULL;
-
-    // mouseCallbackBinding mouseBinding = {
-    //     .type = SDL_MOUSEBUTTONDOWN,
-    //     .button = SDL_BUTTON_LEFT,
-    //     .callback = startUpClicksCB,
-    //     .owner = eng
-    // };
-    // addMouseCallback(&mouseBinding);
-
-    // keyStateBinding ksb = {
-    //     .k = SDLK_SPACE,
-    //     .s = KEYPRESS_STATE_FRAME_PAUSE,
-    //     .t = BINDING_ONE_TIME
-    // };
-    // addBinding(&ksb);
-
-    // ksb.k = SDLK_RIGHT;
-    // ksb.s = KEYPRESS_STATE_FORWARD_FRAME;
-    // ksb.t = BINDING_CONTINUOUS;
-    // addBinding(&ksb);
-
-    // ksb.k = SDLK_LCTRL;
-    // ksb.s = KEYPRESS_STATE_FORWARD_FRAME_MULTIPLIER;
-    // ksb.t = BINDING_CONTINUOUS;
-    // addBinding(&ksb);
 
     eng->mainStateMachine = createHSStateMachine(eng);
 
